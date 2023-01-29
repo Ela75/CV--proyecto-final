138531ad54f7c5f365aa8642ac0ef3b7461b42b9
Ela75 <122704494+Ela75@users.noreply.github.com>
2023-01-29 19:27:05 -0300

    Add files via upload

---
+++ CV/JS/function.js
@@ -0,0 +1,13 @@
+document.getElementById("boton").addEventListener("click", function(){
+
+    console.log("capturamos elemento click");
+    document.getElementById("telefono").innerHTML="(0222)555-4545";
+
+})
+
+document.getElementById("boton_mail").addEventListener("click", function(){
+
+    console.log("capturamos elemento click");
+    document.getElementById("email").innerHTML="JannetteP@gmail.com";
+
+})
